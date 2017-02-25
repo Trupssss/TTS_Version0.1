ba7bda73c9a25e014fb5f410e70be5339c74cfb8
Trupssss <tru.ptiissc@gmail.com>
2017-02-25 16:42:08 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>PHP Starter Application</title>
+	<title>ISSC Tour Guide</title>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 	<link rel="stylesheet" href="style.css" />
 	
